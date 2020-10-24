585caf5b6b46ee1c21f44a9c263863a88eeb6dd4
CharmaineChosen <charmaine.makete@gmail.com>
2020-10-24 14:15:30 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NewsService } from '../news.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  articles: any
 
-  constructor() {}
+  constructor(private NewsService:NewsService) {
+    this.loadNews()
+  }
+  
+
+  loadNews(){
+    this.NewsService.getNews("top-headlines?country=us").subscribe(news => {
+      this.articles = news['articles'];
+      console.log(this.articles);
+    })
+
+  }
 
 }
